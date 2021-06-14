523ab6b0a569d2ef0e1bd76aa913e49c0c1df81b
David Song <davesongpc@gmail.com>
2021-06-14 11:47:35 +0100

    testing more changes

--- random_number_generator.py
+++ random_number_generator.py
@@ -3,6 +3,7 @@ import random
 import itertools
 from typing import List
 #some changes
+#some changes again
 class RandomGen(object):
 
     def __init__(self, random_nums: List[int], probabilities: List[float]):
